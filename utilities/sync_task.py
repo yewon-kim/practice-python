394d9ece5fef9d5800fd5cae33b71114d596aa40
Yewon Kim <yewonkim.dev@gmail.com>
2021-03-02 16:05:02 +0900

    Update sync_task.py

--- utilities/sync_task.py
+++ utilities/sync_task.py
@@ -1,29 +1,18 @@
 import numpy as np
 import pandas as pd
-import threading
+
+codes = ['A000010', 'A000020']
+prices = [[10, 20, 30, 40, 50, 60, 70, 80, 90, 100],
+               [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000]]
+df = pd.DataFrame(prices, index=codes).T
+df_buy = pd.DataFrame([50, 800], index=codes).T
 
 
-class SyncTask:
-    def __init__(self):
-        pass
-
-    def TaskA(self):
-        price = np.random.randint(2000, 30000)
-        buy_at = np.random.randint(2000, 30000)
-
-        if price >= buy_at - 5:
-            print('%s \t %s [매수]' % (price, buy_at))
+for i in range(len(df)):
+    for code in codes:
+        price = df[code][i]
+        buy_at = df_buy[code][0]
+        if (price >= buy_at):
+            print('%s %7s %7s [매수]' % (code, price, buy_at))
         else:
-            print('%s \t %s' % (price, buy_at))
-
-        threading.Timer(1, self.TaskA).start()
-
-
-def main():
-    inst = SyncTask()
-    inst.TaskA()
-
-
-if __name__ == '__main__':
-    main()
-
+            print('%s %7s %7s' % (code, price, buy_at))
